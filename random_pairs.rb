a175838f6856199c6a7ba0dff31936003852768a
Josh Varney <joshcainvarney@gmail.com>
2018-07-09 14:04:30 -0400

    passed all test for grouping and randomizing

--- random_pairs.rb
+++ random_pairs.rb
@@ -3,7 +3,7 @@ def names_array(*names)
 end
 
 def randomized_array(names)
-	# random_array = names.sample(names.count).each_slice(2).to_a
 	random_array = names.each_slice(2).to_a
-	p random_array
+	# random_array = names.sample(names.count).each_slice(2).to_a
+ 	# random_array
 end
--- random_pairs_test.rb
+++ random_pairs_test.rb
@@ -3,27 +3,41 @@ require_relative "random_pairs.rb"
 class Test_Random_pairs < Minitest::Test
 
 	
-	# def test_that_names_array_returns_array
-	# 	assert_equal(Array, names_array("josh").class)
-	# end
-	# def test_that_my_names_array_returns_with_name_in_array
-	# 	assert_equal(["josh"], names_array("josh"))
-	# end
-	# def test_that_my_names_array_puts_all_names_into_array
-	# 	assert_equal(["josh", "john", "kevin", "nick"], names_array("josh", "john", "kevin", "nick"))
-	# end
-	# def test_that_can_randomize_names_array
-	# 	names = "josh", "john", "kevin", "nick"
-	# 	assert_equal(Array, randomized_array(names).class)
-	# end 
-	# def test_that_can_randomize_names_array
-	# 	names = "josh", "john", "kevin", "nick"
-	# 	refute_equal(["josh", "john", "kevin", "nick"], randomized_array(names))
-	# end 
-	def test_that_can_group_in_pairs_without_random_on
-		names = "josh", "john", "kevin", "nick"
-		assert_equal([["josh", "john"],["kevin", "nick"]], randomized_array(names))
+	def test_that_names_array_returns_array
+		assert_equal(Array, names_array("josh").class)
 	end
-
-
+	def test_that_my_names_array_returns_with_name_in_array
+		assert_equal(["josh"], names_array("josh"))
+	end
+	def test_that_my_names_array_puts_all_names_into_array
+		assert_equal(["josh", "john", "kevin", "nick"], names_array("josh", "john", "kevin", "nick"))
+	end
+	def test_that_can_randomize_names_array
+		names = "josh", "john", "kevin", "nick"
+		assert_equal(Array, randomized_array(names).class)
+	end 
+	def test_that_can_randomize_names_array
+		names = "josh", "john", "kevin", "nick"
+		refute_equal(["josh", "john", "kevin", "nick"], randomized_array(names))
+	end 
+	# def test_that_can_group_in_pairs_without_random_on
+	# 	names = "josh", "john", "kevin", "nick"
+	# 	assert_equal([["josh", "john"],["kevin", "nick"]], randomized_array(names))
+	# end
+	# def test_that_will_group_together_more_names_without_random_on
+	# 	names = "josh", "john", "kevin", "nick", "steve", "travis", "trey", "phil"
+	# 	assert_equal([["josh", "john"], ["kevin", "nick"], ["steve", "travis"], ["trey", "phil"]], randomized_array(names))
+	# end
+	# def test_that_will_work_with_odd_number
+	# 	names = "josh", "john", "kevin", "nick", "steve"
+	# 	assert_equal([["josh", "john"],["kevin", "nick"], ["steve"]], randomized_array(names))
+	# end
+	def test_that_can_return_correct_amount_of_arrays_inside_arrays_with_even_elements
+		names = "josh", "john", "kevin", "nick"
+		assert_equal(2, randomized_array(names).count)
+	end
+	def test_that_can_randomize_and_return_correct_number_of_arrays_inside_array
+		names = "josh", "john", "kevin", "nick", "steve"
+		assert_equal(3, randomized_array(names).count)
+	end
 end 
