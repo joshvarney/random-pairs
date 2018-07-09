413cb5ea1bb55a887ecbc901ae33a070893db4b2
Josh Varney <joshcainvarney@gmail.com>
2018-07-09 11:38:48 -0400

    test passed that name returns in array

---
+++ random_pairs.rb
@@ -0,0 +1,6 @@
+def names_array(names)
+	my_names_array = []
+	my_names_array.push(names)
+	p my_names_array
+
+end
--- random_pairs_test.rb
+++ random_pairs_test.rb
@@ -2,7 +2,13 @@ require "minitest/autorun"
 require_relative "random_pairs.rb"
 class Test_Random_pairs < Minitest::Test
 
-
+	
+	def test_that_names_array_returns_array
+		assert_equal(Array, names_array("josh").class)
+	end
+	def test_that_my_names_array_returns_with_name_in_array
+		assert_equal(["josh"], names_array("josh"))
+	end
 
 
 end 
