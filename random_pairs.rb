083d7ef1302405c98f901902c511c148f94028f7
Josh Varney <joshcainvarney@gmail.com>
2018-07-09 13:47:16 -0400

    tested that can group in pairs

--- random_pairs.rb
+++ random_pairs.rb
@@ -1,8 +1,9 @@
 def names_array(*names)
-	p names
+	names
 end
 
-def randomized_array(*names)
-	random_array = names.sample(names.count)
+def randomized_array(names)
+	# random_array = names.sample(names.count).each_slice(2).to_a
+	random_array = names.each_slice(2).to_a
 	p random_array
 end
--- random_pairs_test.rb
+++ random_pairs_test.rb
@@ -3,19 +3,27 @@ require_relative "random_pairs.rb"
 class Test_Random_pairs < Minitest::Test
 
 	
-	def test_that_names_array_returns_array
-		assert_equal(Array, names_array("josh").class)
+	# def test_that_names_array_returns_array
+	# 	assert_equal(Array, names_array("josh").class)
+	# end
+	# def test_that_my_names_array_returns_with_name_in_array
+	# 	assert_equal(["josh"], names_array("josh"))
+	# end
+	# def test_that_my_names_array_puts_all_names_into_array
+	# 	assert_equal(["josh", "john", "kevin", "nick"], names_array("josh", "john", "kevin", "nick"))
+	# end
+	# def test_that_can_randomize_names_array
+	# 	names = "josh", "john", "kevin", "nick"
+	# 	assert_equal(Array, randomized_array(names).class)
+	# end 
+	# def test_that_can_randomize_names_array
+	# 	names = "josh", "john", "kevin", "nick"
+	# 	refute_equal(["josh", "john", "kevin", "nick"], randomized_array(names))
+	# end 
+	def test_that_can_group_in_pairs_without_random_on
+		names = "josh", "john", "kevin", "nick"
+		assert_equal([["josh", "john"],["kevin", "nick"]], randomized_array(names))
 	end
-	def test_that_my_names_array_returns_with_name_in_array
-		assert_equal(["josh"], names_array("josh"))
-	end
-	def test_that_my_names_array_puts_all_names_into_array
-		assert_equal(["josh", "john", "kevin", "nick"], names_array("josh", "john", "kevin", "nick"))
-	end
-	def test_that_can_randomize_names_array
-		refute_equal(["josh", "john", "kevin", "nick"], randomized_array("josh", "john", "kevin", "nick"))
-	end
-
 
 
 end 
