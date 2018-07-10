a2be42f3dd7afe15982f18f868efe0d2d18dfc5d
Josh Varney <joshcainvarney@gmail.com>
2018-07-10 19:16:44 -0400

    all tests passed and code refactored

--- random_pairs.rb
+++ random_pairs.rb
@@ -1,8 +1,5 @@
 # def names_array(*names)
-	# names.count
-	# names << randomized_array(names)
-	# names = names[names.count..-1]
-# 	names
+# 	 names >> randomized_array(names) 
 # end
 def randomized_array(names)
 	random_array = names.sample(names.count).each_slice(2).to_a
@@ -11,8 +8,7 @@ def randomized_array(names)
 		random_array2 = random_array2[0] + random_array2[1]	
 		random_array = random_array[0..-3]
 		random_array.push(random_array2)
-		p random_array
 	else 
-		p random_array
+		random_array
 	end
 end
--- random_pairs_test.rb
+++ random_pairs_test.rb
@@ -3,13 +3,13 @@ require_relative "random_pairs.rb"
 class Test_Random_pairs < Minitest::Test
 
 	
-	# def test_that_names_array_returns_array_NA
+	# def test_that_names_array_returns_array_00
 	# 	assert_equal(Array, names_array("josh").class)
 	# end
-	# def test_that_my_names_array_returns_with_name_in_array_NA
+	# def test_that_my_names_array_returns_with_name_in_array_00
 	# 	assert_equal(["josh"], names_array("josh"))
 	# end
-	# def test_that_my_names_array_puts_all_names_into_array_NA
+	# def test_that_my_names_array_puts_all_names_into_array_00
 	# 	assert_equal(["josh", "john", "kevin", "nick"], names_array("josh", "john", "kevin", "nick"))
 	# end
 	def test_that_can_randomize_names_array_return_array
@@ -28,7 +28,7 @@ class Test_Random_pairs < Minitest::Test
 		names = "josh", "john", "kevin", "nick", "steve", "travis", "trey", "phil"
 		assert_equal(4, randomized_array(names).count)
 	end
-	# def test_that_will_work_with_odd_number_of_names_NA
+	# def test_that_will_work_with_odd_number_of_names_00
 	# 	names = "josh", "john", "kevin", "nick", "steve"
 	# 	assert_equal(3, randomized_array(names).count)
 	# end
@@ -36,7 +36,7 @@ class Test_Random_pairs < Minitest::Test
 		names = "josh", "john", "kevin", "nick"
 		assert_equal(2, randomized_array(names).count)
 	end
-	# def test_that_can_randomize_and_return_correct_number_of_subarrays_NA
+	# def test_that_can_randomize_and_return_correct_number_of_subarrays_00
 	# 	names = "josh", "john", "kevin", "nick", "steve"
 	# 	assert_equal(3, randomized_array(names).count)
 	# end
@@ -52,15 +52,15 @@ class Test_Random_pairs < Minitest::Test
 		names =  "josh", "john", "kevin", "nick", "steve", "travis", "trey", "phil", "jaxon"
 		assert_equal(2, randomized_array(names)[0].count)
 	end
-	# def test_that_returns_1_on_last_array_if_odd_number_of_names_NA
+	# def test_that_returns_1_on_last_array_if_odd_number_of_names_00
 	# 	names = "josh", "john", "kevin", "nick", "steve", "travis", "trey", "phil", "jaxon"
 	# 	assert_equal(1, randomized_array(names)[-1].count)
 	# end
-	# def test_that_can_combine_last_2_arrays_if_odd_NA
+	# def test_that_can_combine_last_2_arrays_if_odd_00
 	# 	names = "josh", "john", "kevin", "nick", "steve", "travis", "trey", "phil", "jaxon"
 	# 	assert_equal(3, randomized_array(names).count)
 	# end
-	# def test_that_can_drop_last_two_off_original_array_NA
+	# def test_that_can_drop_last_two_off_original_array_00
 	# 	names = "josh", "john", "kevin", "nick", "steve", "travis", "trey", "phil", "jaxon"
 	# 	assert_equal(3, randomized_array(names).count)
 	# end
@@ -76,4 +76,4 @@ class Test_Random_pairs < Minitest::Test
 		names = "josh", "john", "kevin", "nick", "steve", "travis", "trey", "phil"
 		assert_equal(4, randomized_array(names).count)
 	end
-end 
+end
